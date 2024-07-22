d55fe3cde4a274f4d126f4949a52c6f6beccad3e
Alexius Andrianno Alfa Renanta <alexiusrenanta@gmail.com>
2024-07-23 04:00:25 +0700

    Add: Chart Building Classes

---
+++ app/src/main/java/com/mobile/itfest/utils/AndroidChartJSView.kt
@@ -0,0 +1,78 @@
+package com.dewakoding.androidchartjs
+
+import android.content.Context
+import android.net.Uri
+import android.util.AttributeSet
+import android.webkit.WebChromeClient
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.widget.LinearLayout
+import com.dewakoding.androidchartjs.model.BubbleEntity
+import com.google.gson.Gson
+import com.mobile.itfest.R
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStream
+import java.io.InputStreamReader
+
+class AndroidChartJSView  @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyle: Int = 0
+) : LinearLayout(context, attrs, defStyle) {
+    private val webView: WebView
+    internal var jsi: JavascriptInterface? = null
+
+    init {
+        orientation = VERTICAL
+        webView = WebView(context)
+    }
+
+    fun setChart(type: String,label: Array<String>, data: Array<Int>, backgroundColor: Array<String>, labelTitle: String) {
+        jsi = JavascriptInterface(getContext(), type, Gson().toJson(label) , Gson().toJson(data), Gson().toJson(backgroundColor), labelTitle)
+        render(jsi)
+    }
+
+    fun setBubbleChart(data: Array<BubbleEntity>, backgroundColor: Array<String>, labelTitle: String) {
+        jsi = JavascriptInterface(context, "bubble", null, Gson().toJson(data), Gson().toJson(backgroundColor), labelTitle)
+        render(jsi)
+    }
+
+    private fun render(jsi: JavascriptInterface?) {
+        val webSettings: WebSettings = webView.getSettings()
+        webSettings.javaScriptEnabled = true
+        webView.addJavascriptInterface(jsi!!, JavascriptInterface.TAG_HANDLER)
+        webView.setWebChromeClient(WebChromeClient())
+
+        val rawResourceId = R.raw.chart2023
+        val inputStream = resources.openRawResource(rawResourceId)
+        val htmlData = convertStreamToString(inputStream)
+        val baseUrl = "file:///android_res/raw/"
+        val dataUri = Uri.parse(baseUrl + rawResourceId)
+
+
+        webView.loadDataWithBaseURL(baseUrl, htmlData, "text/html", "UTF-8", null)
+
+        addView(webView)
+    }
+
+    private fun convertStreamToString(inputStream: InputStream): String {
+        val reader = BufferedReader(InputStreamReader(inputStream))
+        val stringBuilder = StringBuilder()
+        var line: String?
+        try {
+            while (reader.readLine().also { line = it } != null) {
+                stringBuilder.append(line).append("\n")
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        } finally {
+            try {
+                inputStream.close()
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+        return stringBuilder.toString()
+    }
+}
---
+++ app/src/main/java/com/mobile/itfest/utils/JavascriptInterface.kt
@@ -0,0 +1,51 @@
+package com.dewakoding.androidchartjs
+
+import android.content.Context
+import android.webkit.JavascriptInterface
+
+
+class JavascriptInterface(
+    context: Context,
+    type: String,
+    label: String?,
+    data: String,
+    backgroundColor: String,
+    labelTitle: String
+) {
+    companion object {
+        const val TAG_HANDLER = "Android"
+    }
+    private val mType = type
+    private val mLabel = label
+    private val mData = data
+    private val mBackgroundColor = backgroundColor
+    private val mLabelTitle = labelTitle
+
+
+    @JavascriptInterface
+    fun getType(): String {
+        return mType
+    }
+
+    @JavascriptInterface
+    fun getLabel(): String {
+        return mLabel.toString()
+    }
+
+    @JavascriptInterface
+    fun getData(): String {
+        return mData
+    }
+
+    @JavascriptInterface
+    fun getBackgroundColor(): String {
+        return mBackgroundColor
+    }
+
+    @JavascriptInterface
+    fun getLabelTitle(): String {
+        return mLabelTitle
+    }
+
+
+}
