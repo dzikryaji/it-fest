6ff21f954aade96ee1bf1b434008ec832a823762
Alexius Andrianno Alfa Renanta <alexiusrenanta@gmail.com>
2024-08-12 01:54:30 +0700

    Ui Profile

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,8 @@ dependencies {
     //GSON
     implementation(libs.gson)
 
+    implementation (libs.hdodenhof.circleimageview)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
